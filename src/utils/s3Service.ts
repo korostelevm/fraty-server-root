5c6e133df93810694cb994379274745df444d8ec
Anurag Pal <apal895@gmail.com>
2023-02-02 09:52:25 +0530

    migrate 3s to v3s3

--- src/utils/s3Service.ts
+++ src/utils/s3Service.ts
@@ -1,49 +1,69 @@
-import { S3 } from 'aws-sdk';
-import { v4 as uuid } from 'uuid';
-import fs from 'fs';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuid } from "uuid";
+import fs from "fs";
 
-const s3Upload = async (file:any) => {
-    const s3 = new S3();
-    const fileStream = fs.createReadStream(file.filepath);
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `${uuid()}-${file.originalFilename}`,
-        Body: fileStream,
-    }
-  
-    return new Promise((resolve, reject) => {
-        s3.upload(params, (err:any, data:any) => {
-            if (err) {
-                reject(err);
-            }
-            if (data) {
-                resolve(data);
-            }
-        });
-    })
+const s3Upload = async (file: any) => {
+  const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const fileStream = fs.createReadStream(file.filepath);
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuid()}-${file.originalFilename}`,
+    Body: fileStream,
   };
+  const command = new GetObjectCommand({
+    Bucket: params.Bucket,
+    Key: params.Key,
+  });
 
-  const s3MetadataUpload = async (file:any) => {
-    const s3 = new S3();
+  return new Promise((resolve, reject) => {
+    s3.send(command, (err: any, data: any) => {
+      if (err) {
+        reject(err);
+      }
+      if (data) {
+        resolve(data);
+      }
+    });
+  });
+};
 
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `${uuid()}.json`,
-        Body: file,
-        ContentEncoding: 'base64',
-        ContentType: 'application/json'
-    }
-  
-    return new Promise((resolve, reject) => {
-        s3.upload(params, (err:any, data:any) => {
-            if (err) {
-                reject(err);
-            }
-            if (data) {
-                resolve(data);
-            }
-        });
-    })
+const s3MetadataUpload = async (file: any) => {
+  const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuid()}.json`,
+    Body: file,
+    ContentEncoding: "base64",
+    ContentType: "application/json",
   };
+  const command = new GetObjectCommand({
+    Bucket: params.Bucket,
+    Key: params.Key,
+  });
+  return new Promise((resolve, reject) => {
+    s3.send(command, (err: any, data: any) => {
+      if (err) {
+        reject(err);
+      }
+      if (data) {
+        resolve(data);
+      }
+    });
+  });
+};
 
-export { s3Upload,s3MetadataUpload };
+export { s3Upload, s3MetadataUpload };
